a5ed28f1fde0f4eccf4cba264aab1247e29fe52a
Tareq_N <tareq.pro.1996@gmail.com>
2024-12-21 10:57:31 +0200

    Stop tracking bin folder

--- bin/script/environment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTempDirectory = void 0;
-function getTempDirectory() {
-    return process.env.TEMP || process.env.TMPDIR;
-}
-exports.getTempDirectory = getTempDirectory;
